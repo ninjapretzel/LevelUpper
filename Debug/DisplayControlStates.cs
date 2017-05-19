9c4e17eb3262360d9df88083720ef8538b8ffbc3
ninjapretzel <ninjapretzel@yahoo.com>
2017-05-19 13:41:34 -0600

    Moves these behaviours into their own namespace.

--- Debug/DisplayControlStates.cs
+++ Debug/DisplayControlStates.cs
@@ -4,34 +4,39 @@ using System.Collections.Generic;
 using LevelUpper.Extensions;
 using LevelUpper.InputSystem;
 
-public class DisplayControlStates : MonoBehaviour {
+namespace LevelUpper.Debugs {
+	public class DisplayControlStates : MonoBehaviour {
 
 #if DEVELOPMENT_BUILD || UNITY_EDITOR
-	void OnGUI() {
-		GUI.skin = Resources.Load<GUISkin>("standard");
-		GUI.color = Color.white.Alpha(.5f);
+		void OnGUI() {
+			GUI.skin = Resources.Load<GUISkin>("standard");
+			GUI.color = Color.white.Alpha(.5f);
 
-		Rect area = new Rect(0, 0, Screen.width * .5f, Screen.height);
-		GUI.Box(area, "");
-		GUILayout.BeginVertical("box", GUILayout.Width(Screen.width / 2f), GUILayout.Height(Screen.height)); {
-			int i = 0;
-			foreach (var pair in ControlStates.GetAll()) {
-				GUI.color = ((i++ % 2 == 0) ? Color.grey : Color.white).Alpha(.5f);
+			Rect area = new Rect(0, 0, Screen.width * .5f, Screen.height);
+			GUI.Box(area, "");
+			GUILayout.BeginVertical("box", GUILayout.Width(Screen.width / 2f), GUILayout.Height(Screen.height));
+			{
+				int i = 0;
+				foreach (var pair in ControlStates.GetAll()) {
+					GUI.color = ((i++ % 2 == 0) ? Color.grey : Color.white).Alpha(.5f);
 
-				GUILayout.BeginHorizontal("box"); {
-					GUI.color = Color.white;
-					GUILayout.Label(pair.Key);
-					GUILayout.FlexibleSpace();
-					GUILayout.Label(pair.Value);
+					GUILayout.BeginHorizontal("box"); {
+						GUI.color = Color.white;
+						GUILayout.Label(pair.Key);
+						GUILayout.FlexibleSpace();
+						GUILayout.Label(pair.Value);
 
-				} GUILayout.EndHorizontal();
+					}
+					GUILayout.EndHorizontal();
+
+				}
 
 			}
+			GUILayout.EndVertical();
 
-		} GUILayout.EndVertical(); 
 
+		}
+#endif
 
 	}
-#endif 
-
 }
