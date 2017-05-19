103e0c3eb1a17a9f7633fca2785055990b17a2b0
ninjapretzel <ninjapretzel@yahoo.com>
2017-05-19 13:45:13 -0600

    Moves AutodestructSound into LevelUpper.Sound namespace.

--- Systems/Sound/AutodestructSound.cs
+++ Systems/Sound/AutodestructSound.cs
@@ -1,12 +1,16 @@
 using UnityEngine;
 using System.Collections;
 
-public class AutodestructSound : MonoBehaviour {
-	void Awake() { if (transform.parent == null) { DontDestroyOnLoad(gameObject); } }
-	void Start() { if (transform.parent == null) { DontDestroyOnLoad(gameObject); } }
-	void LateUpdate() {
-		if (GetComponent<AudioSource>()) {
-			if (!GetComponent<AudioSource>().isPlaying) { Destroy(gameObject); }
-		} else { Destroy(gameObject); }
+namespace LevelUpper.Sound {
+
+	public class AutodestructSound : MonoBehaviour {
+		void Awake() { if (transform.parent == null) { DontDestroyOnLoad(gameObject); } }
+		void Start() { if (transform.parent == null) { DontDestroyOnLoad(gameObject); } }
+		void LateUpdate() {
+			if (GetComponent<AudioSource>()) {
+				if (!GetComponent<AudioSource>().isPlaying) { Destroy(gameObject); }
+			} else { Destroy(gameObject); }
+		}
 	}
+
 }
