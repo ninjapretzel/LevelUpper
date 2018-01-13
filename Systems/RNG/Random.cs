167e6b5334eddb50ce243e4d78bbe4574df1c056
ninjapretzel <ninjapretzel@yahoo.com>
2018-01-13 00:05:12 -0700

    Separates Random into both an instanced and static class. Static class holds an instance globally.

--- Systems/RNG/Random.cs
+++ Systems/RNG/Random.cs
@@ -4,31 +4,25 @@ using System.Collections.Generic;
 using System;
 
 namespace LevelUpper.RNG {
-
 	/// <summary> Class that provides similar behaviour to the UnityEngine.Random class, but with a more controlled state. </summary>
-	public static class Random {
+	public class Rand {
 		/// <summary> Current Seed Value </summary>
-		public static long seed { get; set; }
+		public long seed { get; set; }
 		/// <summary> Stack of seed history. </summary>
-		private static Stack<long> seeds;
+		private Stack<long> seeds;
 
 		#region Bookkeeping
-		/// <summary> Force a function to run during static initialization </summary>
-		public static bool loaded = Load();
-		/// <summary> Loads this class </summary>
-		/// <returns> always true </returns>
-		static bool Load() {
-			seed = DateTime.Now.Ticks;
+		public Rand() {
+			seed = DateTime.UtcNow.Ticks;
 			seeds = new Stack<long>();
-			return true;
 		}
 
 		/// <summary> Remember the current seed, and change the sequence to use a given seed. </summary>
 		/// <param name="s"> New seed to use </param>
-		public static void Push(long s) { seeds.Push(seed); seed = s; }
+		public void Push(long s) { seeds.Push(seed); seed = s; }
 		/// <summary> Restore the sequence to the last remembered sequence, if there are any </summary>
 		/// <returns> Previous value of <see cref="seed"/> </returns>
-		public static long Pop() {
+		public long Pop() {
 			long s = seed;
 			if (seeds.Count > 0) { seed = seeds.Pop(); } else { Debug.LogWarning("LevelUpper.Random: No seed to pop!"); }
 			return s;
@@ -36,46 +30,70 @@ namespace LevelUpper.RNG {
 
 		/// <summary> Forwards the random class to the next seed value </summary>
 		/// <returns> The next hash value </returns>
-		static long nextSeed { get { return seed = SRNG.hash(seed); } }
+		long nextSeed { get { return seed = SRNG.hash(seed); } }
 		#endregion
 
 		#region Basic Properties
 		/// <summary> Provides a "Random" value, evenly distributed in the range [0, 1) </summary>
-		public static float value { get { return SRNG.Float32(nextSeed); } }
+		public float value { get { return SRNG.Float32(nextSeed); } }
 
 		/// <summary> Provides a "Random" value, evenly distributed in the range [-1, 1) </summary>
-		public static float unit { get { return -1f + (2f * value); } }
+		public float unit { get { return -1f + (2f * value); } }
 
 		/// <summary> Provides a "Random" value, roughly normally distributed in the range [-1, 1) </summary>
 		/// <remarks> Implemented by 3 samples of value, divided by 3. </remarks>
-		public static float normal { get { return (-1 + (2f * (value + value + value) / 3f)); } }
+		public float normal { get { return (-1 + (2f * (value + value + value) / 3f)); } }
 
 		/// <summary> Returns a "Random" Int32 from [0, int.MaxValue) </summary>
-		public static int rawInt { get { return SRNG.Int32(int.MaxValue, nextSeed); } }
+		public int rawInt { get { return SRNG.Int32(int.MaxValue, nextSeed); } }
 		/// <summary> Returns a "Random" byte value from [0, 256) </summary>
-		public static byte rawByte { get { return (byte)rawInt; } }
+		public byte rawByte { get { return (byte)rawInt; } }
 
 		/// <summary> Provides a "Random" character from 'a' to 'z' </summary>
-		public static char alpha { get { return (char)('a' + (int)(value * 26)); } }
+		public char alpha { get { return (char)('a' + (int)(value * 26)); } }
 		/// <summary> Provides a "Random" character from 'A' to 'A' </summary>
-		public static char ALPHA { get { return (char)('A' + (int)(value * 26)); } }
+		public char ALPHA { get { return (char)('A' + (int)(value * 26)); } }
 		/// <summary> Provides a "Random" character from '0' to '9' </summary>
-		public static char numeric { get { return (char)('0' + (int)(value * 10)); } }
+		public char numeric { get { return (char)('0' + (int)(value * 10)); } }
 
 		/// <summary> Gets a "Random" point inside a cube centered at (0, 0, 0) with sides (1, 1, 1) </summary>
-		public static Vector3 insideUnitCube { get { return new Vector3(Range(-.5f, .5f), Range(-.5f, .5f), Range(-.5f, .5f)); } }
+		public Vector3 insideUnitCube { get { return new Vector3(Range(-.5f, .5f), Range(-.5f, .5f), Range(-.5f, .5f)); } }
 		/// <summary> Gets a "Random" point inside a square centered at (0, 0) with sides (1, 1) </summary>
-		public static Vector2 insideUnitSquare { get { return new Vector2(Range(-.5f, .5f), Range(-.5f, .5f)); } }
+		public Vector2 insideUnitSquare { get { return new Vector2(Range(-.5f, .5f), Range(-.5f, .5f)); } }
 
 		/// <summary> Gets a "Random" point inside a sphere centered at (0, 0, 0) with radius 1 </summary>
-		public static Vector3 insideUnitSphere { get { return insideUnitSquare.normalized * value; } }
+		public Vector3 insideUnitSphere { get { return insideUnitSquare.normalized * value; } }
 		/// <summary> Gets a "Random" point inside a circle centered at (0, 0) with radius 1 </summary>
-		public static Vector2 insideUnitCircle { get { return insideUnitSquare.normalized * value; } }
+		public Vector2 insideUnitCircle { get { return insideUnitSquare.normalized * value; } }
 
 		/// <summary> Gets a "Random" point on a sphere centered at (0, 0, 0) with radius 1 </summary>
-		public static Vector3 onUnitSphere { get { return insideUnitCube.normalized; } }
+		public Vector3 onUnitSphere { get { return insideUnitCube.normalized; } }
 		/// <summary> Gets a "Random" point on a circle centered at (0, 0) with radius 1 </summary>
-		public static Vector3 onUnitCircle { get { return insideUnitSquare.normalized; } }
+		public Vector3 onUnitCircle { get { return insideUnitSquare.normalized; } }
+
+		/// <summary> Gets a "Random" color in the RGB channels, with an alpha of 1 </summary>
+		public Color rgb { get { return new Color(value, value, value, 1); } }
+		/// <summary> Gets a "Random" color in the RGBA channels </summary>
+		public Color rgba { get { return new Color(value, value, value, value); } }
+
+		/// <summary> Gets a "Random" Color32 in the RGB channels, with an alpha of 255. </summary>
+		public Color32 RGB { 
+			get { 
+				byte[] bytes = BitConverter.GetBytes(rawInt);
+				return new Color32(bytes[3], bytes[2], bytes[1], 255);
+			}
+		}
+
+		/// <summary> Gets a "Random" Color32 in the RGBA channels </summary>
+		public Color32 RGBA {
+			get {
+				byte[] bytes = BitConverter.GetBytes(rawInt);
+				return new Color32(bytes[3], bytes[2], bytes[1], bytes[0]);
+			}
+		}
+
+		/// <summary> Returns a rotation made by rotating from (-180, 180) on all of X/Y/Z axis </summary>
+		public Quaternion rotation { get { return Quaternion.Euler(insideUnitCube * 360); } }
 		#endregion
 
 		#region Basic Functions
@@ -83,24 +101,24 @@ namespace LevelUpper.RNG {
 		/// <param name="min"> Lower limit, inclusive. </param>
 		/// <param name="max"> Upper limit, not inclusive. </param>
 		/// <returns> "Random" value inside range [<paramref name="min"/>, <paramref name="max"/>) </returns>
-		public static int Range(int min, int max) { return SRNG.Int32Range(min, max, nextSeed); }
+		public int Range(int min, int max) { return SRNG.Int32Range(min, max, nextSeed); }
 		/// <summary> Returns a Float32 in the range [<paramref name="min"/>, <paramref name="max"/>) </summary>
 		/// <param name="min"> Lower limit, inclusive. </param>
 		/// <param name="max"> Upper limit, not inclusive. </param>
 		/// <returns> "Random" value inside range [<paramref name="min"/>, <paramref name="max"/>) </returns>
-		public static float Range(float min, float max) { return SRNG.Float32Range(min, max, nextSeed); }
+		public float Range(float min, float max) { return SRNG.Float32Range(min, max, nextSeed); }
 		/// <summary> Returns a value centered at <paramref name="avg"/> with maximum deviation of +/-<paramref name="dev"/>. </summary>
 		/// <param name="avg"> Average value </param>
 		/// <param name="dev"> Maximum deviation </param>
 		/// <returns> Value in range [<paramref name="avg"/>-<paramref name="dev"/>, <paramref name="avg"/>+<paramref name="dev"/>) </returns>
-		public static float Normal(float avg, float dev) { return avg + (dev * normal); }
+		public float Normal(float avg, float dev) { return avg + (dev * normal); }
 		#endregion
 
 		#region Choosing
 		/// <summary> Chooses an index from a list of weights </summary>
 		/// <param name="weights"> Weights to the given choice </param>
 		/// <returns> Random index from range [0, <paramref name="weights"/>.Count) </returns>
-		public static int WeightedChoose(IList<float> weights) {
+		public int WeightedChoose(IList<float> weights) {
 			float total = 0;
 			int i;
 			for (i = 0; i < weights.Count; i++) { total += weights[i]; }
@@ -114,6 +132,99 @@ namespace LevelUpper.RNG {
 			return weights.Count - 1;
 		}
 
+		#endregion
+
+	}
+
+
+
+	/// <summary> Class that provides similar behaviour to the UnityEngine.Random class, but with a more controlled state. </summary>
+	public static class Random {
+		/// <summary> Instance of random object </summary>
+		private static Rand instance = new Rand();
+
+		/// <summary> Remember the current seed, and change the sequence to use a given seed. </summary>
+		/// <param name="s"> New seed to use </param>
+		public static void Push(long s) { instance.Push(s); }
+		/// <summary> Restore the sequence to the last remembered sequence, if there are any </summary>
+		/// <returns> Previous value of <see cref="seed"/> </returns>
+		public static long Pop() { return instance.Pop(); }
+		
+		#region Basic Properties
+		/// <summary> Provides a "Random" value, evenly distributed in the range [0, 1) </summary>
+		public static float value { get { return instance.value; } }
+
+		/// <summary> Provides a "Random" value, evenly distributed in the range [-1, 1) </summary>
+		public static float unit { get { return instance.unit; } }
+
+		/// <summary> Provides a "Random" value, roughly normally distributed in the range [-1, 1) </summary>
+		/// <remarks> Implemented by 3 samples of value, divided by 3. </remarks>
+		public static float normal { get { return instance.normal; } }
+
+		/// <summary> Returns a "Random" Int32 from [0, int.MaxValue) </summary>
+		public static int rawInt { get { return instance.rawInt; } }
+		/// <summary> Returns a "Random" byte value from [0, 256) </summary>
+		public static byte rawByte { get { return instance.rawByte; } }
+
+		/// <summary> Provides a "Random" character from 'a' to 'z' </summary>
+		public static char alpha { get { return instance.alpha; } }
+		/// <summary> Provides a "Random" character from 'A' to 'A' </summary>
+		public static char ALPHA { get { return instance.ALPHA; } }
+		/// <summary> Provides a "Random" character from '0' to '9' </summary>
+		public static char numeric { get { return instance.numeric; } }
+
+		/// <summary> Gets a "Random" point inside a cube centered at (0, 0, 0) with sides (1, 1, 1) </summary>
+		public static Vector3 insideUnitCube { get { return instance.insideUnitCube; } }
+		/// <summary> Gets a "Random" point inside a square centered at (0, 0) with sides (1, 1) </summary>
+		public static Vector2 insideUnitSquare { get { return instance.insideUnitSquare; } }
+
+		/// <summary> Gets a "Random" point inside a sphere centered at (0, 0, 0) with radius 1 </summary>
+		public static Vector3 insideUnitSphere { get { return instance.insideUnitSphere; } }
+		/// <summary> Gets a "Random" point inside a circle centered at (0, 0) with radius 1 </summary>
+		public static Vector2 insideUnitCircle { get { return insideUnitCircle; } }
+
+		/// <summary> Gets a "Random" point on a sphere centered at (0, 0, 0) with radius 1 </summary>
+		public static Vector3 onUnitSphere { get { return instance.onUnitSphere; } }
+		/// <summary> Gets a "Random" point on a circle centered at (0, 0) with radius 1 </summary>
+		public static Vector3 onUnitCircle { get { return instance.onUnitCircle; } }
+
+		/// <summary> Gets a "Random" color in the RGB channels, with an alpha of 1 </summary>
+		public static Color rgb { get { return instance.rgb; } }
+		/// <summary> Gets a "Random" color in the RGBA channels </summary>
+		public static Color rgba { get { return instance.rgba; } }
+
+		/// <summary> Gets a "Random" Color32 in the RGB channels, with an alpha of 255. </summary>
+		public static Color32 RGB { get { return instance.RGB; } }
+		/// <summary> Gets a "Random" Color32 in the RGBA channels </summary>
+		public static Color32 RGBA { get { return instance.RGBA; } }
+
+		/// <summary> Returns a rotation made by rotating from (-180, 180) on all of X/Y/Z axis </summary>
+		public static Quaternion rotation { get { return instance.rotation; } }
+		#endregion
+
+		#region Basic Functions
+		/// <summary> Returns an Int32 in the range [<paramref name="min"/>, <paramref name="max"/>) </summary>
+		/// <param name="min"> Lower limit, inclusive. </param>
+		/// <param name="max"> Upper limit, not inclusive. </param>
+		/// <returns> "Random" value inside range [<paramref name="min"/>, <paramref name="max"/>) </returns>
+		public static int Range(int min, int max) { return instance.Range(min, max); }
+		/// <summary> Returns a Float32 in the range [<paramref name="min"/>, <paramref name="max"/>) </summary>
+		/// <param name="min"> Lower limit, inclusive. </param>
+		/// <param name="max"> Upper limit, not inclusive. </param>
+		/// <returns> "Random" value inside range [<paramref name="min"/>, <paramref name="max"/>) </returns>
+		public static float Range(float min, float max) { return instance.Range(min, max); }
+		/// <summary> Returns a value centered at <paramref name="avg"/> with maximum deviation of +/-<paramref name="dev"/>. </summary>
+		/// <param name="avg"> Average value </param>
+		/// <param name="dev"> Maximum deviation </param>
+		/// <returns> Value in range [<paramref name="avg"/>-<paramref name="dev"/>, <paramref name="avg"/>+<paramref name="dev"/>) </returns>
+		public static float Normal(float avg, float dev) { return instance.Normal(avg, dev); }
+		#endregion
+
+		#region Choosing
+		/// <summary> Chooses an index from a list of weights </summary>
+		/// <param name="weights"> Weights to the given choice </param>
+		/// <returns> Random index from range [0, <paramref name="weights"/>.Count) </returns>
+		public static int WeightedChoose(IList<float> weights) { return instance.WeightedChoose(weights); }
 		#endregion
 
 	}
