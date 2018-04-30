8935d2589b147faab91b49bef8a52eb04a9764bc
ninjapretzel <ninjapretzel@yahoo.com>
2018-04-29 20:37:45 -0600

    Adds folder for ScriptableObjects, adds some simple scriptable objects

---
+++ Systems/SOs/FloatRef.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName="NewFloatRef", menuName="Vars/Create New FloatRef")]
+public class FloatRef : ScriptableObject {
+	public float Constant;
+	public FloatVar Variable;
+	public float value { get { return Variable != null ? Variable.value : Constant; } }
+}
---
+++ Systems/SOs/FloatVar.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "NewFloatVar", menuName = "Vars/Create New FloatVar")]
+public class FloatVar : ScriptableObject {
+	public float value;
+}
